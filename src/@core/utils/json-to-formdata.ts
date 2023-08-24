c429b6dd5a4d30fa24d07c1ed38f4aa10189015b
RO03M <romera.gv.s@gmail.com>
2023-08-23 22:17:39 -0300

    feat: adding comma

--- src/@core/utils/json-to-formdata.ts
+++ src/@core/utils/json-to-formdata.ts
@@ -2,7 +2,7 @@ export const jsonToFormData = (json: any) => {
     const formData = new FormData();
 
     for (var key in json) {
-        formData.append(key, json[key])
+        formData.append(key, json[key]);
     }
 
     return formData;
