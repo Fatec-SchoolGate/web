33f6aa0bbffaa9e5139b3cecb090e210bd91cc72
RO03M <romera.gv.s@gmail.com>
2023-08-23 22:19:29 -0300

    feat: moved organizationContainer

--- src/views/organization/index.tsx
+++ src/views/organization/index.tsx
@@ -1,19 +1,11 @@
-import { Box, Card } from "@mui/material";
-import Header from "./Header";
-import Form from "./Form";
-import SubjectList from "./SubjectList";
+import { Box } from "@mui/material";
+import SubjectSpace from "./SubjectSpace";
 
 const OrganizationContainer = () => {
     return (
-        <Card
-            sx={{
-                p: (theme) => theme.spacing(4)
-            }}
-        >
-            <Header/>
-            <SubjectList/>
-            <Form/>
-        </Card>
+        <Box>
+            <SubjectSpace/>
+        </Box>
     );
 }
 
