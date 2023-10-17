91066cd0917bd8ff9169b8f53095fdaa66494638
gabriel.romera <gabriel.romera@hit.com.vc>
2023-10-17 19:20:05 -0300

    feat: removed organization page tabs

--- src/views/organizations/OrganizationsContainer.tsx
+++ src/views/organizations/OrganizationsContainer.tsx
@@ -1,17 +1,10 @@
-import { Card, Tab, Tabs } from "@mui/material";
-import { useState } from "react";
+import { Card } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import SwipeableViews from 'react-swipeable-views';
 import YourOrganizationList from "./YourOrganizations/YourOrganizationList";
 import Header from "./Header";
 
 const OrganizationsContainer = () => {
-
-    const [tabValue, setTabValue] = useState<any>(0);
-
     const { t } = useTranslation();
-
-    const changeTab = (_: any, value: any) => setTabValue(value);
 
     return (
         <Card
@@ -20,25 +13,7 @@ const OrganizationsContainer = () => {
             }}
         >
             <Header/>
-            <Tabs
-                value={tabValue}
-                onChange={changeTab}
-            >
-                <Tab
-                    value={0}
-                    label={t("yourOrganizations")}
-                />
-                <Tab
-                    value={1}
-                    label={t("invitedOrganizations")}
-                />
-            </Tabs>
-            <SwipeableViews
-                index={tabValue}
-                onChange={(index) => changeTab(null, index)}
-            >
-                <YourOrganizationList/>
-            </SwipeableViews>
+            <YourOrganizationList/>
         </Card>
     );
 }
